0651c3f74027a4a016032637dc7bfe6965d4f6d9
Adarsh Vyas <adarshvyas99@gmail.com>
2025-07-16 17:07:39 +0530

    resolve tfsec CRITICAL issues

--- infrastructure/modules/security_groups/main.tf
+++ infrastructure/modules/security_groups/main.tf
@@ -18,7 +18,7 @@ resource "aws_security_group" "alb_sg" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] # tfsec:ignore:aws-ec2-no-public-ingress-sgr
   }
 
   egress {
